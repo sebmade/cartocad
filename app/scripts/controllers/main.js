2278b747230bb4da8f1ae50f1bffa5e72ef463ff
Sebastien Letelie <sletelie@improve.fr>
2012-11-17 15:29:10 +0100

    final

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,4 +4,11 @@ cartocadApp.controller('MainCtrl', function($scope, $http) {
   $http.get("data/articles.json").success(function(data) {
     $scope.articles = data;
   });
+  $scope.cart = [];
+  $scope.addToCart = function(article) {
+    $scope.cart.push(article);
+  };
+  $scope.total = function() {
+    return _.reduce($scope.cart, function(memo, a){ return memo + parseFloat(a.price); }, 0);
+  }
 });
